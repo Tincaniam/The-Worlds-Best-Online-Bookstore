0256a314d9347722bb19879db8bf18906b088c39
mtinnel <matttinnel@gmail.com>
2023-05-05 17:16:15 -0700

    Basic Tables/Forms added

--- client/src/pages/CustomersPage.js
+++ client/src/pages/CustomersPage.js
@@ -5,6 +5,82 @@ export const CustomersPage = () => {
     return (
         <div>
             <h3>Customers</h3>
+            <br />
+            <h5>Add Customer</h5>
+            <input
+                type="text"
+                placeholder="first_name"
+                />
+            <input
+                type="text"
+                placeholder="last_name"
+                />
+            <input
+                type="text"
+                placeholder="address"
+                />
+            <input
+                type="text"
+                placeholder="email_address"
+                />
+            <input
+                type="text"
+                placeholder="phone_number"
+                />
+            <button className="button-medium">Add Author</button>
+
+            <br /><br />
+            <table className="table table-striped">
+                <thead>
+                    <tr>
+                        <th>customer_id</th>
+                        <th>first_name</th>
+                        <th>last_name</th>
+                        <th>address</th>
+                        <th>email_address</th>
+                        <th>phone_number</th>
+                        <th>actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Freddy</td>
+                        <td>Lupin</td>
+                        <td>1111 Wolftone Dr, New York, NY 10001</td>
+                        <td>flupin@email.com</td>
+                        <td>5038675309</td>
+                        <td>
+                            <button className="btn btn-outline-primary">Edit</button>
+                            <button className="btn btn-outline-danger">Delete</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Bob</td>
+                        <td>Bobberson</td>
+                        <td>4567 Street Dr, Oregon City, OR 97045</td>
+                        <td>bbobberson@email.com</td>
+                        <td>NULL</td>
+                        <td>
+                            <button className="btn btn-outline-primary">Edit</button>
+                            <button className="btn btn-outline-danger">Delete</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td>Haily</td>
+                        <td>Sharp</td>
+                        <td>22365 S Wall St, Oregon City, OR 97045</td>
+                        <td>hsharp@email.com</td>
+                        <td>NULL</td>
+                        <td>
+                            <button className="btn btn-outline-primary">Edit</button>
+                            <button className="btn btn-outline-danger">Delete</button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     );
 }
